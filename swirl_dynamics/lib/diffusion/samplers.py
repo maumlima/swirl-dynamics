d5f9a052e418195d2eb73e6392ef1dd19bfefd65
maumlima <mauriciodemouralima@gmail.com>
2024-08-02 15:18:34 +0200

    add custom sampler

--- swirl_dynamics/lib/diffusion/samplers.py
+++ swirl_dynamics/lib/diffusion/samplers.py
@@ -506,3 +506,264 @@ class SdeSampler(Sampler):
       return jnp.sqrt(dsquare_sigma_dt) * self.scheme.scale(t)
 
     return sde.SdeDynamics(_drift, _diffusion)
+
+
+@flax.struct.dataclass
+class CustomSampler:
+  """Base class for noising-denoising-based diffusion samplers from an original sample.
+
+  Attributes:
+    input_shape: The tensor shape of a sample (excluding any batch dimensions).
+    scheme: The diffusion scheme which contains the scale and noise schedules.
+    denoise_fn: A function to remove noise from input data. Must handle batched
+      inputs, noise levels and conditions.
+    tspan: Full diffusion time steps for iterative denoising, decreasing from 1
+      to (approximately) 0.
+    guidance_transforms: An optional sequence of guidance transforms that
+      modifies the denoising function in a post-process fashion.
+    apply_denoise_at_end: If `True`, applies the denoise function another time
+      to the terminal states, which are typically at a small but non-zero noise
+      level.
+    return_full_paths: If `True`, the output of `.generate()` and `.denoise()`
+      will contain the complete sampling paths. Otherwise only the terminal
+      states are returned.
+  """
+
+  input_shape: tuple[int, ...]
+  scheme: diffusion.Diffusion
+  denoise_fn: DenoiseFn
+  tspan: Array
+  guidance_transforms: Sequence[guidance.Transform] = ()
+  apply_denoise_at_end: bool = True
+  return_full_paths: bool = False
+
+  def generate(
+      self,
+      init_sample: Array,
+      num_samples: int,
+      rng: Array,
+      cond: ArrayMapping | None = None,
+      guidance_inputs: ArrayMapping | None = None,
+  ) -> Array:
+    """Generates a batch of diffusion samples from scratch.
+
+    Args:
+    init_sample: The initial sample from which to generate the diffusion samples.
+      num_samples: The number of samples to generate in a single batch.
+      rng: The base rng for the generation process.
+      cond: Explicit conditioning inputs for the denoising function. These
+        should be provided **without** batch dimensions (one should be added
+        inside this function based on `num_samples`).
+      guidance_inputs: Inputs used to construct the guided denoising function.
+        These also should in principle not include a batch dimension.
+
+    Returns:
+      The generated samples.
+    """
+    if self.tspan is None or self.tspan.ndim != 1:
+      raise ValueError("`tspan` must be a 1-d array.")
+
+    x0 = jnp.stack([init_sample] * num_samples, axis=0)
+
+    if cond is not None:
+      cond = jax.tree.map(lambda x: jnp.stack([x] * num_samples, axis=0), cond)
+
+    noise_rng, denoise_rng = jax.random.split(rng)
+
+    # Forward pass
+    x1 = self.noise(x0, noise_rng, self.tspan[::-1])
+
+    # Backward pass
+    denoised = self.denoise(x1, denoise_rng, self.tspan, cond, guidance_inputs)
+
+    samples = denoised[-1] if self.return_full_paths else denoised
+    if self.apply_denoise_at_end:
+      denoise_fn = self.get_guided_denoise_fn(guidance_inputs=guidance_inputs)
+      samples = denoise_fn(
+          jnp.divide(samples, self.scheme.scale(self.tspan[-1])),
+          self.scheme.sigma(self.tspan[-1]),
+          cond,
+      )
+      if self.return_full_paths:
+        denoised = jnp.concatenate([denoised, samples[None]], axis=0)
+
+    return denoised if self.return_full_paths else samples
+
+  def noise(
+      self,
+      x0: Array,
+      rng: Array,
+      tspan: Array,
+  ) -> Array:
+    """Applies iterative denoising to given noisy states.
+
+    Args:
+      x0: A batch of the initial sample num_samples times.
+      rng: Base Jax rng for denoising.
+      tspan: An increasing sequence of diffusion time steps within the interval
+      (0, t_star].
+
+    Returns:
+      The noised output.
+    """
+    raise NotImplementedError
+
+  def denoise(
+      self,
+      noisy: Array,
+      rng: Array,
+      tspan: Array,
+      cond: ArrayMapping | None,
+      guidance_inputs: ArrayMapping | None,
+  ) -> Array:
+    """Applies iterative denoising to given noisy states.
+
+    Args:
+      noisy: A batch of noisy states (all at the same noise level). Can be fully
+        noisy or partially denoised.
+      rng: Base Jax rng for denoising.
+      tspan: A decreasing sequence of diffusion time steps within the interval
+        [t_star, 0). The first element aligns with the time step of the `noisy`
+        input.
+      cond: (Optional) Conditioning inputs for the denoise function. The batch
+        dimension should match that of `noisy`.
+      guidance_inputs: Inputs for constructing the guided denoising function.
+
+    Returns:
+      The denoised output.
+    """
+    raise NotImplementedError
+
+  def get_guided_denoise_fn(
+      self, guidance_inputs: Mapping[str, Array]
+  ) -> DenoiseFn:
+    """Returns a guided denoise function."""
+    denoise_fn = self.denoise_fn
+    for transform in self.guidance_transforms:
+      denoise_fn = transform(denoise_fn, guidance_inputs)
+    return denoise_fn
+
+
+@flax.struct.dataclass
+class SdeCustomSampler(CustomSampler):
+  """Draws samples by solving the forward and then the backward SDE from a given sample.
+
+  Attributes:
+    integrator: The SDE solver for solving the sampling SDE.
+  """
+
+  integrator: sde.SdeSolver = sde.EulerMaruyama(iter_type="scan")
+
+  def noise(
+      self,
+      x0: Array,
+      rng: Array,
+      tspan: Array,
+  ) -> Array:
+    """Applies iterative noising to given initial states."""
+    if self.integrator.terminal_only and self.return_full_paths:
+      raise ValueError(
+          f"Integrator type `{type(self.integrator)}` does not support"
+          " returning full paths."
+      )
+
+    params = dict(
+        drift={}, diffusion={}
+    )
+    denoised = self.integrator(self.forward_dynamics, x0, tspan, rng, params)
+    # SDE solvers may return either the full paths or the terminal state only.
+    # If the former, the lead axis should be time.
+    samples = denoised if self.integrator.terminal_only else denoised[-1]
+    return denoised if self.return_full_paths else samples
+
+  @property
+  def forward_dynamics(self) -> sde.SdeDynamics:
+    """Drift and diffusion terms of the forward SDE.
+
+    Following Karras et al. (https://arxiv.org/abs/2206.00364):
+
+      dx = ṡ(t)/s(t) x dt + s(t) √[2σ̇(t)σ(t)] dωₜ,
+
+    where s(t), σ(t) are the scale and noise schedule of the diffusion scheme
+    respectively.
+    """
+
+    def _drift(x: Array, t: Array, params: Params) -> Array:
+      assert not t.ndim, "`t` must be a scalar."
+      dlog_s_dt = dlog_dt(self.scheme.scale)(t)
+      drift = dlog_s_dt * x
+      return drift
+
+    def _diffusion(x: Array, t: Array, params: Params) -> Array:
+      del x, params
+      assert not t.ndim, "`t` must be a scalar."
+      dsquare_sigma_dt = dsquare_dt(self.scheme.sigma)(t)
+      return jnp.sqrt(dsquare_sigma_dt) * self.scheme.scale(t)
+
+    return sde.SdeDynamics(_drift, _diffusion)
+
+  def denoise(
+      self,
+      noisy: Array,
+      rng: Array,
+      tspan: Array,
+      cond: ArrayMapping | None = None,
+      guidance_inputs: ArrayMapping | None = None,
+  ) -> Array:
+    """Applies iterative denoising to given noisy states."""
+    if self.integrator.terminal_only and self.return_full_paths:
+      raise ValueError(
+          f"Integrator type `{type(self.integrator)}` does not support"
+          " returning full paths."
+      )
+
+    params = dict(
+        drift=dict(guidance_inputs=guidance_inputs, cond=cond), diffusion={}
+    )
+    denoised = self.integrator(self.backward_dynamics, noisy, tspan, rng, params)
+    # SDE solvers may return either the full paths or the terminal state only.
+    # If the former, the lead axis should be time.
+    samples = denoised if self.integrator.terminal_only else denoised[-1]
+    return denoised if self.return_full_paths else samples
+
+  @property
+  def backward_dynamics(self) -> sde.SdeDynamics:
+    """Drift and diffusion terms of the backward SDE.
+
+    In score function:
+
+      dx = [ṡ(t)/s(t) x - 2 s(t)²σ̇(t)σ(t) ∇pₜ(x)] dt + s(t) √[2σ̇(t)σ(t)] dωₜ,
+
+    obtained by substituting eq. 28, 34 of Karras et al.
+    (https://arxiv.org/abs/2206.00364) into the reverse SDE formula - eq. 6 in
+    Song et al. (https://arxiv.org/abs/2011.13456). Alternatively, it may be
+    rewritten in terms of the denoise function (plugging in eq. 74 of
+    Karras et al.) as:
+
+      dx = [2 σ̇(t)/σ(t) + ṡ(t)/s(t)] x - [2 s(t)σ̇(t)/σ(t)] D(x/s(t), σ(t)) dt
+        + s(t) √[2σ̇(t)σ(t)] dωₜ
+
+    where s(t), σ(t) are the scale and noise schedule of the diffusion scheme
+    respectively.
+    """
+
+    def _drift(x: Array, t: Array, params: Params) -> Array:
+      assert not t.ndim, "`t` must be a scalar."
+      denoise_fn = self.get_guided_denoise_fn(
+          guidance_inputs=params["guidance_inputs"]
+      )
+      s, sigma = self.scheme.scale(t), self.scheme.sigma(t)
+      x_hat = jnp.divide(x, s)
+      dlog_sigma_dt = dlog_dt(self.scheme.sigma)(t)
+      dlog_s_dt = dlog_dt(self.scheme.scale)(t)
+      drift = (2 * dlog_sigma_dt + dlog_s_dt) * x
+      drift -= 2 * dlog_sigma_dt * s * denoise_fn(x_hat, sigma, params["cond"])
+      return drift
+
+    def _diffusion(x: Array, t: Array, params: Params) -> Array:
+      del x, params
+      assert not t.ndim, "`t` must be a scalar."
+      dsquare_sigma_dt = dsquare_dt(self.scheme.sigma)(t)
+      return jnp.sqrt(dsquare_sigma_dt) * self.scheme.scale(t)
+
+    return sde.SdeDynamics(_drift, _diffusion)
